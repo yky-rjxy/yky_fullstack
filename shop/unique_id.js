8522e278c7fb45f94f2bb890aca103bb11f60012
yky-rjxy <52236827+yky-rjxy@users.noreply.github.com>
2021-07-11 18:49:46 +0800

    skr-shop 抖音电商 订单 模块化

---
+++ shop/unique_id.js
@@ -0,0 +1,15 @@
+/**
+ * 获得唯一ID
+ * @author yky
+ * @date 20-11-21
+ * @return {string}
+ */
+// const uniqueID = () => {
+//     let t = + new Date(); //类型转换
+//     let ranNum = Math.ceil(Math.random()*10000)
+//     return t + "" + ranNum;
+// }
+const uniqueID = () => + new Date() + '' + Math.ceil(Math.random()*10000); 
+
+
+console.log(uniqueID());
