aa5b133f0000773f4be1066e8521e0ea1520eff1
yky-rjxy <52236827+yky-rjxy@users.noreply.github.com>
2021-07-11 17:04:43 +0800

    TwoSum leetcode 腾讯必考题

---
+++ leetcode/twoSum/2.js
@@ -0,0 +1,30 @@
+/**
+ * @author yky
+ * @date 20-11-20
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// 箭头函数
+const twoSum = (nums, target) => {
+    // console.log('两数相加')
+    let map = {}; // JSON Object
+    let res = [];
+    // 编写功能 一个函数完成 一个功能
+    // es6更简洁
+    // callback
+    // for + 执行功能
+    nums.forEach((e, i) => map[e] = i); // O(n) 以空间换时间
+    // console.log(map)
+    // ?
+    for (let i = 0; i < nums.length; i++) {
+        let j = map[target-nums[i]]; // undefined
+        if(j && j !==i){
+            res = [i, j];
+            break;
+        }
+    }
+    return res;
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
---
+++ leetcode/twoSum/readme.md
@@ -0,0 +1,28 @@
+# TwoSum leetcode 腾讯必考题
+
+1. node 有安装好吗？
+    - node.exe  programfiles/
+    - node 全局可访问
+    gt
+- node 是让js 运行在服务器端的环境，
+    开发速度 更快， 没有java那么稳定
+    Server Linux  pwd 当前在哪里
+    node 1.js
+
+
+- 读题
+    整数数组 nums, target 参数
+    返回 下标
+    const  nums = [2, 7, 11, 15]
+
+- 前期了leetcode 先暴力再调优 easy
+    1. 时间复杂度
+        O(n2)
+    2. 空间复杂度
+
+- 提供一个算法
+    O(n2) -> O(n)
+    找一种适合的数据结构 
+    Array 也是最基本的数据结构 是一个数据集合 数字索引，空间是连续的
+    JSON Object对象字面量 也是一个数据 集合  在c里没有原生的， hashMap
+    O(n2) -> O(n) + 空间多花
